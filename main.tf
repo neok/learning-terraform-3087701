2bd71985c44893c5173d148333e932dd38209a1d
neok <p.popelyshko@gmail.com>
2024-03-19 20:51:02 +0100

    security groups

--- main.tf
+++ main.tf
@@ -14,11 +14,51 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
+data "aws_vpc" "default" {
+  default = true
+}
+
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
+  vpc_security_group_ids = [aws_security_group.web.id]
+
   tags = {
     Name = "HelloWorld"
   }
+}
+
+resource "aws_security_group" "web" {
+  name        = "web"
+  description = "Allow http and https in. Allow everything out"
+
+  vpc_id = data.aws_vpc.default.id
+}
+
+resource "aws_security_group_rule" "web_http_in" {
+  from_port         = 80
+  protocol          = "tcp"
+  security_group_id = aws_security_group.web.id
+  to_port           = 80
+  type              = "ingress"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "web_https_in" {
+  from_port         = 443
+  protocol          = "tcp"
+  security_group_id = aws_security_group.web.id
+  to_port           = 443
+  type              = "ingress"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "web_everything_out" {
+  from_port         = 0
+  protocol          = "-1"
+  security_group_id = aws_security_group.web.id
+  to_port           = 0
+  type              = "egress"
+  cidr_blocks       = ["0.0.0.0/0"]
 }
